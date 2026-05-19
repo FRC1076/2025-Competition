8c933d923a4bc91fafca2d164a049b3757658e44
Tejas Gupta <320948@aaps.k12.mi.us>
2026-05-18 21:27:48 -0400

    Whoops

--- src/main/java/frc/robot/commands/drive/DirectDriveToPose.java
+++
@@ -1,8 +0,0 @@
-package frc.robot.commands.drive;
-
-import edu.wpi.first.math.controller.ProfiledPIDController;
-import edu.wpi.first.wpilibj2.command.Command;
-
-public class DirectDriveToPose extends Command {
-    private final ProfiledPIDController velocityController = new ProfiledPIDController(0, 0, 0, null);
-}
